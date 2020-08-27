b52b0048abcb72476ed2da1bdc41ecee8a271ecb
Aline Borges <ap.borges@live.com>
2020-08-27 20:36:44 -0300

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//import { StyledComponentsModule } from 'angular-styled-components';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
